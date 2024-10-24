6aa5906501c9e2104211b64600625f912304dd9d
Nicholas Pijpers <61523024+Kikolator@users.noreply.github.com>
2024-10-24 16:22:33 +0200

    update front end

--- public/my/flutter_bootstrap.js
+++ public/my/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2949273247"
+    serviceWorkerVersion: "3018219177"
   }
 });
